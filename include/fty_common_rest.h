40c50c7a4c5117192524ed99173e46cc84ffda6e
Degott, Francois Regis <FrancoisRegisDegott@eaton.com>
2021-04-27 09:06:04 +0200

    remove useless typedefs
    Signed-off-by: Degott, Francois Regis <FrancoisRegisDegott@eaton.com>

--- include/fty_common_rest.h
+++ include/fty_common_rest.h
@@ -21,21 +21,7 @@
 
 #pragma once
 
-//  Opaque class structures to allow forward references
-//  These classes are stable or legacy and built in all releases
-typedef struct _fty_common_rest_helpers_t fty_common_rest_helpers_t;
-#define FTY_COMMON_REST_HELPERS_T_DEFINED
-typedef struct _fty_common_rest_sasl_t fty_common_rest_sasl_t;
-#define FTY_COMMON_REST_SASL_T_DEFINED
-typedef struct _fty_common_rest_tokens_t fty_common_rest_tokens_t;
-#define FTY_COMMON_REST_TOKENS_T_DEFINED
-typedef struct _fty_common_rest_utils_web_t fty_common_rest_utils_web_t;
-#define FTY_COMMON_REST_UTILS_WEB_T_DEFINED
-typedef struct _fty_common_rest_audit_log_t fty_common_rest_audit_log_t;
-#define FTY_COMMON_REST_AUDIT_LOG_T_DEFINED
-
-
-//  Public classes, each with its own header file
+//  Public classes
 #include "fty_common_rest_helpers.h"
 #include "fty_common_rest_sasl.h"
 #include "fty_common_rest_tokens.h"
